2b7cb22f4d694ab2473b9efd046d4ead73183191
rubendariogomezsarmiento <rubendariogomezsarmiento@localhost>
2023-09-12 12:53:23 -0400

    Primer commit

---
+++ src/main/java/primero/Nueva.java
@@ -0,0 +1,45 @@
+
+package primero;
+
+import javax.swing.*;
+
+public class Nueva {
+  
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String genero = "";
+		
+		do {
+			
+			genero=JOptionPane.showInputDialog("introduce tu genero (H/M)");
+			
+			
+		}while(genero.equalsIgnoreCase("H")==false && genero.equalsIgnoreCase("M")==false);
+		
+		int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura por favor en cm"));
+		
+		int pesoideal=0;
+		
+		
+		if (genero.equalsIgnoreCase("H")) {
+			
+			pesoideal=altura-110;
+			
+		}
+		
+		else if (genero.equalsIgnoreCase("M")) {
+			
+			pesoideal=altura-120;
+			
+		}
+		
+		
+		System.out.println("tu peso ideal es " + pesoideal + "kg") ;
+		
+
+	}
+    
+    
+}
