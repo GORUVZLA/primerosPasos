6d41fb2a86385df1628aa9263f77f987611f8350
rubendariogomezsarmiento <rubendariogomezsarmiento@localhost>
2023-09-12 12:31:09 -0400

    Primer commit

---
+++ src/main/java/com/mycompany/primerospasos/Primerospasos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerospasos;
+
+/**
+ *
+ * @author rubendariogomezsarmiento
+ */
+public class Primerospasos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello culo!");
+    }
+}
